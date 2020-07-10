ae129b122c9ebe38d8f532c356b5b71cc2e40457
keishibuki <kei.play@gmail.com>
2020-07-10 15:39:52 +0900

    Add: React環境構築

--- culsite/functions.php
+++ culsite/functions.php
@@ -7,7 +7,7 @@
  */
 function setup_theme() {
 	add_editor_style( array(
-		'./assets/css/admin-editor-style.css', 
+		'./assets/css/admin-editor-style.css',
 	) );
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'title-tag' );
@@ -27,42 +27,42 @@ function my_wp_enqueue_scripts() {
 	$dir_path = get_stylesheet_directory_uri() . '/assets';
 
 	$args = array(
-		'handle' => 'theme-normalize', 
-		'src'    => $dir_path . '/css/sanitize.css', 
-		'deps'   => array(), 
-		'ver'    => false, 
-		'media'  => 'all' 
+		'handle' => 'theme-normalize',
+		'src'    => $dir_path . '/css/sanitize.css',
+		'deps'   => array(),
+		'ver'    => false,
+		'media'  => 'all'
 	);
 	extract($args);
 	wp_enqueue_style( $handle, $src, $deps, $ver, $media );
 
 	$args = array(
-		'handle' => 'theme-common', 
-		'src'    => $dir_path . '/css/common.css', 
-		'deps'   => array(), 
-		'ver'    => false, 
-		'media'  => 'all' 
+		'handle' => 'theme-common',
+		'src'    => $dir_path . '/css/common.css',
+		'deps'   => array(),
+		'ver'    => false,
+		'media'  => 'all'
 	);
 	extract($args);
 	wp_enqueue_style( $handle, $src, $deps, $ver, $media );
 
 	$args = array(
-		'handle' => 'theme', 
-		'src'    => get_stylesheet_uri(), 
-		'deps'   => array(), 
-		'ver'    => false, 
-		'media'  => 'all' 
+		'handle' => 'theme',
+		'src'    => get_stylesheet_uri(),
+		'deps'   => array(),
+		'ver'    => false,
+		'media'  => 'all'
 	);
 	extract($args);
 	wp_enqueue_style( $handle, $src, $deps, $ver, $media );
 
 	if( is_home() || is_front_page() ) {
 		$args = array(
-			'handle' => 'home', 
-			'src'    => $dir_path . '/css/home.css', 
-			'deps'   => array(), 
-			'ver'    => false, 
-			'media'  => 'all', 
+			'handle' => 'home',
+			'src'    => $dir_path . '/css/home.css',
+			'deps'   => array(),
+			'ver'    => false,
+			'media'  => 'all',
 		);
 		extract($args);
 		wp_enqueue_style( $handle, $src, $deps, $ver, $media );
@@ -70,11 +70,11 @@ function my_wp_enqueue_scripts() {
 
 	if( is_page() && !is_front_page() ) {
 		$args = array(
-			'handle' => 'page-style', 
-			'src'    => $dir_path . '/css/page.css', 
-			'deps'   => array(), 
-			'ver'    => false, 
-			'media'  => 'all' 
+			'handle' => 'page-style',
+			'src'    => $dir_path . '/css/page.css',
+			'deps'   => array(),
+			'ver'    => false,
+			'media'  => 'all'
 		);
 		extract($args);
 		wp_enqueue_style( $handle, $src, $deps, $ver, $media );
@@ -82,44 +82,56 @@ function my_wp_enqueue_scripts() {
 
 	if( is_single() || is_singular() && !is_front_page() ) {
 		$args = array(
-			'handle' => 'entry-style', 
-			'src'    => $dir_path . '/css/entry.css', 
-			'deps'   => array(), 
-			'ver'    => false, 
-			'media'  => 'all' 
+			'handle' => 'entry-style',
+			'src'    => $dir_path . '/css/entry.css',
+			'deps'   => array(),
+			'ver'    => false,
+			'media'  => 'all'
 		);
 		extract($args);
 		wp_enqueue_style( $handle, $src, $deps, $ver, $media );
 	}
 
 	$args = array(
-		'handle' => 'jquery.matchHeight-min.js', 
-		'src'    => $dir_path . '/scripts/jquery.matchHeight-min.js', 
-		'deps'   => array('jquery'), 
-		'ver'    => false, 
-		'in_footer' => true, 
+		'handle' => 'jquery.matchHeight-min.js',
+		'src'    => $dir_path . '/scripts/jquery.matchHeight-min.js',
+		'deps'   => array('jquery'),
+		'ver'    => false,
+		'in_footer' => true,
 	);
 	extract($args);
 	wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 
 	$args = array(
-		'handle' => 'ofi.min.js', 
-		'src'    => $dir_path . '/scripts/ofi.min.js', 
-		'deps'   => array('jquery'), 
-		'ver'    => false, 
-		'in_footer' => true, 
+		'handle' => 'ofi.min.js',
+		'src'    => $dir_path . '/scripts/ofi.min.js',
+		'deps'   => array('jquery'),
+		'ver'    => false,
+		'in_footer' => true,
 	);
 	extract($args);
 	wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 
 	$args = array(
-		'handle' => 'myscripts.js', 
-		'src'    => $dir_path . '/scripts/myscripts.js', 
-		'deps'   => array('jquery'), 
-		'ver'    => false, 
-		'in_footer' => true, 
+		'handle' => 'myscripts.js',
+		'src'    => $dir_path . '/scripts/myscripts.js',
+		'deps'   => array('jquery'),
+		'ver'    => false,
+		'in_footer' => true,
 	);
 	extract($args);
 	wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+
+	if ( is_page( 'sample-page' ) ) {
+		$args = array(
+			'handle' => 'app.js',
+			'src'    => get_theme_file_uri( '/dist/app.js' ),
+			'deps'   => array('jquery'),
+			'ver'    => false,
+			'in_footer' => true,
+		);
+		extract($args);
+		wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+	}
 }
 add_action('wp_enqueue_scripts', 'my_wp_enqueue_scripts', 11);
