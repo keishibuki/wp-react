91368609c61aaba278fae1cf2ba945d95c5bdf4b
keishibuki <kei.play@gmail.com>
2020-07-14 11:44:18 +0900

    Fix: custom hook useBrand

--- culsite/app/src/hooks/useBrand.js
+++ culsite/app/src/hooks/useBrand.js
@@ -2,13 +2,16 @@ import { useState, useEffect } from 'react';
 
 export default () => {
   const [brands, setBrands] = useState();
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
 
   const fetchBrands = async () => {
     try {
+      setLoading(true);
       const res = await fetch('http://detailbase.local/wp-json/wp/v2/brand');
       const data = await res.json();
       setBrands(data);
+      setLoading(false);
 
       return data;
     } catch (e) {
@@ -25,5 +28,5 @@ export default () => {
     // eslint-disable-next-line
   }, []);
 
-  return { error, brands };
+  return { error, loading, brands };
 };
